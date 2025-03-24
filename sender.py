1a2b6fae1bfb7d157aca6cf5a555122af2cba6e1
szymon <szymon.pt4sznik@gmail.com>
2025-03-24 21:57:05 +0100

    method for merging pdfs into one

--- sender.py
+++ sender.py
@@ -1,6 +1,34 @@
 import os
+import pathlib as pl
+
+from PyPDF2 import PdfWriter, PdfFileReader
+
 
 class Sender:
 
     def __init__(self, dir_path):
         self.dir_path = dir_path
+
+    def get_file_list(self):
+        files = os.listdir(self.dir_path)
+        files.sort(key = lambda x: os.path.getctime(os.path.join(self.dir_path, x)))
+        return [os.path.join(self.dir_path, f) for f in files if f.endswith('.pdf')]
+
+    def merge_files(self):
+        files = self.get_file_list()
+        merger = PdfWriter()
+        for f in files:
+            with open(f, "rb") as page:
+                merger.append(page)
+            os.remove(f)
+
+        with open(os.path.join(self.dir_path, "output.pdf"), "wb") as output:
+            merger.write(output)
+
+        return os.path.join(self.dir_path, "output.pdf")
+
+if __name__ == '__main__':
+    p = pl.Path(os.getcwd())
+    path = os.path.join(p.home(), 'Pictures', 'Scans')
+    s = Sender(path)
+    s.merge_files()
